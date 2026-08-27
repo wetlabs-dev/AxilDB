83ae44c404d3961f76f48fee28f2dfc70c165b4a
Damon Curry <xcode@damonius.com>
2026-08-27 15:09:22 -0400

    Revert "Add iPhone camera specimen photo capture"
    This reverts commit 4300fac3f40edb50aea4a7c32346cd37ca504c46.

--- app/instances/[id]/page.tsx
+++ app/instances/[id]/page.tsx
@@ -465,7 +465,7 @@ export default async function InstanceDetail({
             <input type="hidden" name="entityId" value={id} />
             <input type="hidden" name="collectionSlug" value={collection.slug} />
             <input type="hidden" name="back" value={collectionPath(collection.slug, `/instances/${id}`)} />
-            <PhotoFramingEditor fileInputName="photo" enablePhoneCapture />
+            <PhotoFramingEditor fileInputName="photo" />
             <Field label="Caption" name="caption" />
             <Button>Upload photo</Button>
           </form>
